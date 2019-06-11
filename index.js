713be29cc0768f0e408f5ed62bf1651dab25a4ef
Kajal Goel <kajal11122000@gmail.com>
2019-06-11 13:58:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  const a= document.querySelector(selector);
+  return a;
+}
 
+function nestedTarget(){
+  const b = document.querySelector("#nested .target");
+  return b;
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+    const c = parseInt(lis[i].innerHTML, 10);
+    lis[i].innerHTML = c+n;
+  }
+}
+
+function deepestChild(){
+  const c = document.querySelector("div#grand-node");
+  const d = c.querySelectorAll("div") ;
+  const e = d[d.length - 1];
+  return e;
+}
