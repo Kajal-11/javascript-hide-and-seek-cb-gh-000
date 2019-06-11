2e01037952fb8e03b8e9a20474515c0177290033
Kajal Goel <kajal11122000@gmail.com>
2019-06-11 13:43:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  const a= document.querySelector(selector);
+  return a;
+}
 
+function nestedTarget(){
+  const b = document.querySelector("#nested .target");
+  return b;
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+    const c = parseInt(lis[i].innerHTML, 10);
+    lis[i].innerHTML = c+n;
+  }
+}
+
+function deepestChild(){
+  const c = document.querySelector("div#grand-node")
+  const d= c;
+  for(let j=0; j< 5; j++)
+  {
+    d = d.querySelector("div");
+  }
+  return d;
+}
