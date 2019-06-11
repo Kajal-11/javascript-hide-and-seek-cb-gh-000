bcfe5d7bc1ec66d40faaf879762f394497eeae38
Kajal Goel <kajal11122000@gmail.com>
2019-06-11 13:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,6 @@ function increaseRankBy(n){
 function deepestChild(){
   const c = document.querySelector("div#grand-node");
   const d = c.querySelectorAll("div") ;
-  const e = "/n" + d[d.length - 1].innerHTML + "/n"; 
+  const e = "/n" + d[d.length - 1].innerHTML + "/n";
+  return e;
 }
